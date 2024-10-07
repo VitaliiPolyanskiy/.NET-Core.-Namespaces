f2ae04b8a32e6973c498e9f458dd8f95c79f396d
Admin <polyanskiy@itstep.academy>
2024-10-07 15:48:25 +0300

    Add project files.

---
+++ Namespaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MyNamespaceA.MyNamespaceB;
+using static System.Console;
+using Alias = MyNamespaceA.MyNamespaceB;
+using AliasABTest = MyNamespaceA.MyNamespaceB.Test;
+
+namespace MyNamespaceA
+{
+    namespace MyNamespaceB
+    {
+        class Test
+        {
+            public static void SomeMethod()
+            {
+                System.Console.WriteLine("Test");
+                Console.WriteLine("Test");
+                WriteLine("Test");
+            }
+        }
+    }
+}
+
+namespace C
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            MyNamespaceA.MyNamespaceB.Test.SomeMethod();
+            Test.SomeMethod(); 
+            Alias.Test.SomeMethod();
+            AliasABTest.SomeMethod();
+        }
+    }
+}
+
